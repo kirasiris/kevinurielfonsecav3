1a07da63d711ab2e69f850fec432b1536e849b82
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2022-07-02 22:53:58 -0500

    Fixed title attribute in layout

--- layout/Layout.js
+++ layout/Layout.js
@@ -33,11 +33,7 @@ const Layout = ({
 	return (
 		<>
 			<Head>
-				{title && (
-					<title>
-						{APP_NAME} | {title}
-					</title>
-				)}
+				{title && <title>{APP_NAME + " | " + title}</title>}
 				{description && (
 					<meta name="description" content={`${description}`}></meta>
 				)}
